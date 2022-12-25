02a9ea6d6a5bdafc575e42e4dc37abfed08f0a0f
sol-A1 <118661914+sol-A1@users.noreply.github.com>
2022-12-25 13:30:17 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5002', {
+    const response = await fetch('https://new61.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
